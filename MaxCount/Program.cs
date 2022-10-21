bcdc82c60fde47cb2f7a9f9b72b7b8219531c410
Марат <@DESKTOP-LLV2BAH>
2022-10-21 18:07:36 +0300

    Добавьте файлы проекта.

---
+++ MaxCount/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Введите что нибудь ...");
+string a = Console.ReadLine();
+char[] s = a.ToCharArray();
+int count = 0;
+int maxcount = 0;
+foreach (char c in s)
+{
+    if (char.IsDigit(c))
+    {
+        count++;
+        if (maxcount <= count)
+        {
+            maxcount = count;
+        }
+    }
+    else if (char.IsDigit(c) == false)
+    {
+        count = 0;
+        continue;
+    }
+}
+Console.WriteLine($"Максимальная последовательность чисел - {maxcount}");
